5f20ed3694037309e3bb7a4646fc3d7ddf67b046
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2022-08-31 16:08:23 +0430

    add simple fetching

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
+    const [movies, setMovies] = useState([]);
 
+    const fetchMovieHandler = async () => {
+        const response = await fetch('https://swapi.dev/api/films');
+        const data = await response.json();
+        const transformedMovies = data.results.map((movieData) => {
+            return {
+                id: movieData.episode_id,
+                title: movieData.title,
+                openingText: movieData.opening_crawl,
+                releaseDate: movieData.release_date
+            }
+        })
+        setMovies(transformedMovies);
+    }
 
     return (
         <React.Fragment>
             <section>
-                <button>Fetch Movies</button>
+                <button onClick={fetchMovieHandler}>Fetch Movies</button>
             </section>
             <section>
-                {/*<MoviesList movies={dummyMovies} />*/}
+                <MoviesList movies={movies} />
             </section>
         </React.Fragment>
     );
